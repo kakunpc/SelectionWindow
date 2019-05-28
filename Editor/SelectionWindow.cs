bf2cb9d733d2cb31a1efbf4d5a12d286abc9582d
kakunpc <15257475+kakunpc@users.noreply.github.com>
2019-05-28 16:38:31 +0900

    fix

--- Editor/SelectionWindow.cs
+++ Editor/SelectionWindow.cs
@@ -9,7 +9,7 @@ using UnityEditor.SceneManagement;
 using UnityEngine;
 using System.Collections.Generic;
 
-namespace kakunpc
+namespace kakunpc.SelectionWindow
 {
     public class SelectionWindow : EditorWindow
     {
